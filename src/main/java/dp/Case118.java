918bcedae7dcc2989c0a3649d4d90b8cd0b8b5e7
ryann.chen <ryann.chen@shopee.com>
2021-12-08 15:47:40 +0800

    case 96

---
+++ src/main/java/dp/Case118.java
@@ -0,0 +1,15 @@
+package dp;
+
+import java.util.List;
+
+/**
+ * 118. 杨辉三角
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ */
+public class Case118 {
+    public List<List<Integer>> generate(int n) {
+        return null;
+    }
+}
