bc226544ee1ece8bb897b006dfde90a917d6bfb7
ryann.chen <ryann.chen@shopee.com>
2023-01-04 16:01:00 +0800

    case 322

--- src/main/java/leetcode/dp/Case322.java
+++ src/main/java/leetcode/dp/Case322.java
@@ -1,5 +1,9 @@
 package leetcode.dp;
 
+import utils.Utils;
+
+import java.util.Arrays;
+
 /**
  * 322. 零钱兑换
  * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
@@ -17,8 +21,39 @@ package leetcode.dp;
  * 输出：0
  */
 public class Case322 {
+    public static void main(String[] args) {
+        int[] coins = new int[]{1, 2, 5};
+        int amount = 11;
+        Case322 case322 = new Case322();
+        int res = case322.coinChange(coins, amount);
+        System.out.println(res);
+    }
 
     public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        // dp[i] 表示到达i所需的最少的硬币的个数
+        // dp[i] =Math.min( dp[i-1], dp[i-2], dp[i-5]) + 1
+        // 先默认全是-1
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                dp[coins[i]] = 1;
+            }
+        }
+        Utils.print1dArr(dp);
+        for (int i = 1; i <= amount; i++) {
+            if (dp[i] != -1) continue;
+            int tempMin = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if ((coins[j] < i) && (dp[i - coins[j]] != -1)) {
+                    tempMin = Math.min(dp[i - coins[j]] + 1, tempMin);
+                    dp[i] = tempMin;
+                }
+            }
 
+        }
+        Utils.print1dArr(dp);
+        return dp[amount];
     }
 }
--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -3,16 +3,25 @@ package utils;
 public class Utils {
     /**
      * 输出二维数组
+     *
      * @param mat
      */
     public static void print2dArr(int[][] mat) {
         int row = mat.length;
         int col = mat[0].length;
-        for (int i = 0; i < row; i++){
+        for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void print1dArr(int[] mat) {
+        int row = mat.length;
+        for (int j = 0; j < row; j++) {
+            System.out.print(mat[j] + " ");
+        }
+        System.out.println();
     }
 }
