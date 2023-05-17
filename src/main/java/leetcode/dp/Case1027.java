9e6c6a83a57b6260b16331c282f37c85cccc019e
ryann.chen <ryann.chen@shopee.com>
2023-05-17 18:10:27 +0800

    最长等差子序列的长度 解决

--- src/main/java/leetcode/dp/Case1027.java
+++ src/main/java/leetcode/dp/Case1027.java
@@ -2,13 +2,13 @@ package leetcode.dp;
 
 import utils.Utils;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * * 1027. 最长等差数列
  * 给你一个整数数组 nums，返回 nums 中最长等差子序列的长度。
- *
  * 回想一下，nums 的子序列是一个列表 nums[i1], nums[i2], ..., nums[ik] ，
  * * 且 0 <= i1 < i2 < ... < ik <= nums.length - 1。
  * * 并且如果 seq[i+1] - seq[i]( 0 <= i < seq.length - 1) 的值都相同，那么序列 seq 是等差的。
@@ -38,19 +38,23 @@ public class Case1027 {
     public int longestArithSeqLength(int[] nums) {
         int n = nums.length;
         int[][] dp = new int[n][n];
-        Map<Integer, Integer> map = new HashMap<>();
-        int maxLen = 2;
-        for (int j = 0; j < n; j++) {// k i j
-            map.put(nums[j], j);
-            for (int i = 0; i < j; i++) {
-                int target = 2 * nums[i] - nums[j];
-                if (map.containsKey(target)) {
-                    int k = map.get(target);
-                    dp[j][k] = dp[i][j] + 1;
-                    maxLen = Math.max(maxLen, dp[j][k]);
+        // dp[i][j] 表示nums[i] 和 nums[j] 作为结尾的最长等差子序列的长度，其中 0 ≤ i < j < n
+        int res = 2;
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], 2); // 任意两个数都是等差数列， 所以dp全部设为2
+        }
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < j; i++) { // 选定2个数 做结尾，往前倒， 找到构成等差的所有，然后比长度
+                int diff = nums[j] - nums[i];
+                for (int k = i - 1; k >= 0; k--) { // 这个思路好  最清晰
+                    if (nums[k] == nums[i] - diff) {
+                        dp[i][j] = Math.max(dp[i][j], dp[k][i] + 1);
+                        break;
+                    }
                 }
+                res = Math.max(res, dp[i][j]);
             }
         }
-        return maxLen;
+        return res;
     }
 }
