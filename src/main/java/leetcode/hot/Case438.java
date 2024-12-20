a8b558653c89059237b5bd16351b8319fc4903e1
ryann.chen <ryann.chen@shopee.com>
2024-12-20 18:01:31 +0800

    438

--- src/main/java/leetcode/hot/Case438.java
+++ src/main/java/leetcode/hot/Case438.java
@@ -1,6 +1,7 @@
 package leetcode.hot;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 //438. 找到字符串中所有字母异位词
 //        给定两个字符串 s 和 p，找到 s 中所有 p 的
@@ -23,8 +24,77 @@ import java.util.List;
 //        1 <= s.length, p.length <= 3 * 104
 //        s 和 p 仅包含小写字母
 public class Case438 {
-    public List<Integer> findAnagrams(String s, String p) {
 
-        return null;
+    public static void main(String[] args) {
+        Case438 case438 = new Case438();
+        case438.findAnagrams1("cbaebabacd", "abc");
+                                //0123456789
+
+    }
+
+    public List<Integer> findAnagrams1(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
+        int[] pCheck = new int[26];
+        int[] sCheck = new int[26];
+        for (int i = 0; i < p.toCharArray().length; i++) {
+            char c = p.charAt(i);
+            pCheck[c - 'a'] += 1;
+            char v = s.charAt(i);
+            sCheck[v - 'a'] += 1;
+        }
+
+        if (Arrays.equals(pCheck, sCheck)) {
+            result.add(0);
+        }
+
+        for (int delete = 0; delete < sLen - pLen; delete++) {
+            int add = delete + pLen;
+            sCheck[s.charAt(delete) - 'a'] -= 1;
+            sCheck[s.charAt(add) - 'a'] += 1;
+//            if (sCheck[s.charAt(delete) - 'a'] == pCheck[s.charAt(delete) - 'a'] && sCheck[s.charAt(add) - 'a'] == pCheck[s.charAt(add) - 'a']) {
+            // 这个思路是错的
+            if(Arrays.equals(pCheck, sCheck)) {
+                result.add(delete+1);
+                System.out.println( delete+1);
+
+            }
+        }
+        return result;
+    }
+
+    public boolean check(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
+        for (int i = 0; i < sLen - pLen + 1; i++) {
+            String temp = s.substring(i, i + pLen);
+            if (isChcek(temp, p)) {
+                result.add(i);
+                System.out.println(i);
+            }
+
+        }
+        return result;
+    }
+
+    public boolean isChcek(String a, String b) {
+        // 对字符串内部字符进行排序
+        char[] chars = a.toCharArray();
+        Arrays.sort(chars); // 字符数组排序
+        String sorted = new String(chars); // 排序后转为字符串
+        char[] bc = b.toCharArray();
+        Arrays.sort(bc); // 字符数组排序
+        String sortedb = new String(bc); // 排序后转为字符串
+        return sorted.equals(sortedb);
     }
 }
