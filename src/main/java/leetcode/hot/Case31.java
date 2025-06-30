227d88aef620f7deab7aaab58b8d4d3c67ac925e
Run <rainchen940@gmail.com>
2025-06-30 20:09:57 +0800

    31 写一下¸ª排序

--- src/main/java/leetcode/hot/Case31.java
+++ src/main/java/leetcode/hot/Case31.java
@@ -1,4 +1,7 @@
 package leetcode.hot;
+
+import utils.Utils;
+
 //31. 下一个排列
 //中等
 //相关标签
@@ -16,4 +19,67 @@ package leetcode.hot;
 //
 //必须 原地 修改，只允许使用额外常数空间。
 public class Case31 {
+    public void nextPermutation(int[] nums) {
+        //1 2   3 100 99 6 2      下一个排列是1 2   6 2 3 99 100
+        //      .        .        第一个3（从后往前看，第一个相邻的递减序列）是需要交换的位置，需要交换为6(从后往前第一个大于3的)， 然后把后面的序列全部逆序
+        //1 2   6 100 99 3 2   ->  1 2   6 2 3 99 100
+
+        // 其实可以这样思考，因为题目要找下一个排列
+        // 那么排序其实有规律的，就像我们枚举排列的时候都是从小到大的
+        // 那么其实我们从后往前看，只要找到第一个降序的序列，比如 1 2 3 5 4 的 5 3 是降序的
+        // 但找到5 3 之后，我们并不能直接交换5 3的位置，因为这样不是下一个排列
+        // 所以我们可以从5到后面的数进行从小到大的排序即4 5，
+        // 将这个升序序列中找到第一个大于3的数即4，交换就好了， 得到1 2 4 3 5
+
+        // 其实就是从后往前找，找到第一个降序的序列
+        int len = nums.length;
+        int start = len - 2;
+        while (start >= 0 && nums[start] >= nums[start + 1]) {
+            start--;
+        }
+
+        System.out.println("start:" + start);
+        // 1,2,3,4,3,2,1    start 2  是数字3的位置
+        if (start == -1) {
+            // 整体已经全部是递减的了，整体都需要逆序
+        } else {
+            // 从后往前，找到第一个大于 nums[start]的位置,这里一定会有一个比他大的 ， 不然就是整体递减了，在if的分支里面
+            for (int j = len - 1; j > start; j--) {
+                if (nums[j] > nums[start]) {
+                    swapArray(nums, start, j);
+                    break;
+                }
+            }
+        }
+
+        reverseArray(nums, start+1);
+
+    }
+
+    // 逆序的典型写法
+    public void reverseArray(int[] nums, int start){
+        int left = start;
+        int right = nums.length - 1;
+
+        while(left < right) {
+            swapArray(nums, left, right);
+            left++;
+            right--;
+        }
+
+    }
+
+    public void swapArray(int[] nums, int left, int right){
+        int t = nums[left];
+        nums[left] = nums[right];
+        nums[right] = t;
+    }
+
+
+    public static void main(String[] args) {
+        Case31 case31 = new Case31();
+        int[] nums = {1,2,3,4,5};
+        case31.nextPermutation(nums);
+        Utils.print1dArr(nums);
+    }
 }
